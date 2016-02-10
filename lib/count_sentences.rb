e9013488405a236f617d0ff0ab09475099a97e26
jprosario <jomi0388@gmail.com>
2016-02-10 11:15:05 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    self.split(/[.;?!]/).compact.reject { |e| e.to_s == '' }.count
   end
 end
