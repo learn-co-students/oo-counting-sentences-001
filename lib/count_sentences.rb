2771c7182fdc80d67bf32f625d5a453edf3733a5
lwadya <lukaswadya@yahoo.com>
2016-03-01 23:59:24 -0500

    Completed String class

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    all_sentences = self.split(/[\.+!+?+]/)
+    all_sentences.reject{|x| x.empty?}.size
   end
 end
