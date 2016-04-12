5d0d01ce29af4803ca2d6a61f5f7f24b06413082
fhernanez173 <f.hernandez173@gmail.com>
2016-04-12 17:44:29 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # binding.pry
+    sentences = self.split(/\!(?=\s)|\.(?=\s)|\?(?=\s)/)
+    # binding.pry
+    sentences.length
   end
 end
+
+#split at either '. ' or '! ' or '? '
